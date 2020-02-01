9a6b44ab069e79dffbbe4c1d70a97707cd7ee364
serj <kraspvp@mail.ru>
2020-02-02 00:21:19 +0700

    Добавил кнопки перелистывания контента

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -14,7 +14,13 @@ $this->registerCss("
 ");
 ?>
 
-<h1><?= Html::encode($title);?></h1>
-<?= Html::img(Html::encode($img), ['alt' => Html::encode($title)]); ?>
-<?= Html::button('Нравится', ['class' => 'like_button', 'id' => 'like_button']); ?>
-<?= Html::button('Не нравится', ['class' => 'dislike_button', 'id' => 'dislike_button']); ?>
+<div>
+  <h1><?= Html::encode($title);?></h1>
+  <?= Html::img(Html::encode($img), ['alt' => Html::encode($title)]); ?>
+  <div style="display: block; margin-left: auto; margin-right: auto; max-width: 260px; padding-top: 15px;">
+    <?= Html::button('<<', ['id' => 'prev', 'style' => 'display: inline-block; padding-right: 10px; color: black; cursor: pointer; font-weight: bold;']); ?>
+    <?= Html::button('Нравится', ['class' => 'like_button', 'id' => 'like_button']); ?>
+    <?= Html::button('Не нравится', ['class' => 'dislike_button', 'id' => 'dislike_button']); ?>
+    <?= Html::button('>>', ['id' => 'next', 'style' => 'display: inline-block;  padding-left: 10px; color: black; cursor: pointer; font-weight: bold;']); ?>
+  </div>
+</div>
