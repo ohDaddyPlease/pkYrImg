9cfe7274413fbc47100d2687cf805542a1746010
serj <kraspvp@mail.ru>
2020-02-03 23:50:07 +0700

    Написал вход, выход и регистрацию

--- controllers/AuthorizationController.php
+++ controllers/AuthorizationController.php
@@ -12,6 +12,8 @@ class AuthorizationController extends Controller
     if(!Yii::$app->request->post())
       return $this->goHome();
       $request = Yii::$app->request->post('LoginForm');
+      $identity = User::findOne(['login' => $request['login']]);
+      Yii::$app->user->login($identity);
       return $this->redirect(Yii::$app->request->referrer);
   }
 
@@ -24,12 +26,14 @@ class AuthorizationController extends Controller
     $user->login = $request['login'];
     $user->password = Yii::$app->security->generatePasswordHash($request['password']);
     $user->save();
+    $identity = User::findOne(['login' => $request['login']]);
+    Yii::$app->user->login($identity);
     return $this->redirect(Yii::$app->request->referrer);
   }
 
   public function actionLogout()
   {
-    if(!Yii::$app->request->post())
-    return $this->goHome();
+    Yii::$app->user->logout();
+    return $this->redirect(Yii::$app->request->referrer);
   }
 }
