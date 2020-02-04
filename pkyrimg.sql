b9d522c528245c4a89b96f0f6c6519e7ef69cf1a
Serj <kraspvp@mail.ru>
2020-02-04 16:53:41 +0700

    Добавил таблицу для лайков/дизлайков

--- pkyrimg.sql
+++ pkyrimg.sql
@@ -24,6 +24,30 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `pkyrimg` /*!40100 DEFAULT CHARACTER SE
 USE `pkyrimg`;
 
 --
+-- Table structure for table `assessment`
+--
+
+DROP TABLE IF EXISTS `assessment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `assessment` (
+  `grade` tinyint(1) NOT NULL,
+  `img` varchar(100) NOT NULL,
+  `user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`grade`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assessment`
+--
+
+LOCK TABLES `assessment` WRITE;
+/*!40000 ALTER TABLE `assessment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `assessment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -37,7 +61,7 @@ CREATE TABLE `user` (
   `role` varchar(255) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +70,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'123','$2y$13$0OW8A85O8Op6spE/RgaEaeuyPrHlNXaAYAaeouST0iKkCgV8FE3Xq','user'),(8,'tester','$2y$13$SVBUCLRvGLxKmSc2AZo/aerGKDKonXQWUDyGFVMcREhcYKmnBYvx2','user');
+INSERT INTO `user` VALUES (1,'123','$2y$13$0OW8A85O8Op6spE/RgaEaeuyPrHlNXaAYAaeouST0iKkCgV8FE3Xq','user'),(8,'tester','$2y$13$SVBUCLRvGLxKmSc2AZo/aerGKDKonXQWUDyGFVMcREhcYKmnBYvx2','user'),(9,'pew','$2y$13$LQ2vmQTXxlC1Gqv7nF.GeOg87k/Ma5bg37/jKNePpl0SZJe4Xo736','user'),(11,'ow','$2y$13$2chxep9rG1q3HpRpjvcSpuithfbhOPfaCjgkiJF2zCK5VzYqm1Cpm','user'),(13,'TESTY','$2y$13$RmtOm4g5HajxyNonxtJdh.1x0jWEy4bUCrdigKLR/4NORuAivVV7e','user'),(14,'тест','$2y$13$iCfPMldwO.uHTmFHo3DKWO8lKLTCD0SrvSqBdu71cY.GNtWyPUqqe','user'),(15,'test123','$2y$13$lJLaZoSxEzUZrV4QjU8L7ePNqwehgphkT.PQNY81bYqaw0HfGHpxu','user');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -59,4 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-03 16:14:36
+-- Dump completed on 2020-02-04 16:52:21
