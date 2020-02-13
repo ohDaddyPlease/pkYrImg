5eb6b426ef3f3698998d9b5c449d1563ab7372a6
Serj <kraspvp@mail.ru>
2020-02-13 14:23:55 +0700

    Сделал тестовую прогонку по всем лайкам пользователя

--- views/profile/index.php
+++ views/profile/index.php
@@ -1,2 +1,17 @@
 <?php
  use yii\helpers\Html;
+ use app\models\db\Like;
+$user_id = Yii::$app->user->identity->id;
+$likes =   Like::find()
+                 ->where("user_id = $user_id")
+                 ->all();
+
+$posts = '';
+foreach($likes as $like)
+{
+  $posts .= ' ' . $like->post_id;
+}
+?>
+
+<p>Вы лайкнули следующие посты: </p>
+<p><?= $posts ?></p>
