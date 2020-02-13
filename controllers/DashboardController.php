d7f5ff98b0c18583f2a9e08557133cd2ec4b74f6
Serj <kraspvp@mail.ru>
2020-02-13 14:15:45 +0700

    Добавил действие для добавления лайка в БД (тестовое действие)

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,7 @@
 namespace app\controllers;
 use yii\web\Controller;
 use Yii;
+use app\models\db\Like;
 
 class DashboardController extends Controller
 {
@@ -9,8 +10,18 @@ class DashboardController extends Controller
     {
         $resp = Yii::$app->picker->pick();
         $img =  $resp['img'];
+        $num = $resp['num'];
         $title = $resp['safe_title']??'Pick Your Image';
 
-        return $this->render('index', ['img' => $img, 'title' => $title]);
+        return $this->render('index', ['img' => $img, 'title' => $title, 'num' => $num]);
+    }
+
+    public function actionTest()
+    {
+        $like = new Like;
+        $like->user_id = Yii::$app->user->identity->id;
+        $like->post_id = $_POST['num'];
+        $like->action = $_POST['action'];
+        $like->save();
     }
 }
