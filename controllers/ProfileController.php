2fc6789c71c9a61f545f019d321bb5f892ce0b19
Serj <kraspvp@mail.ru>
2020-02-14 15:43:25 +0700

    Добавил запрет на посещение страницы профиля, если пользователь не авторизован

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -9,6 +9,7 @@ class ProfileController extends Controller
 {
   public function actionIndex()
   {
+    if(Yii::$app->user->isGuest) return $this->goHome(); 
     return $this->render('index.php');
   }
 }
