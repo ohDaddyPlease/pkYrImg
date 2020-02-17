9b6dba808b168d567950142d48d14966e1cb5166
Serj <kraspvp@mail.ru>
2020-02-17 12:05:49 +0700

    Изменена система наведения мышкой на картинки: теперь картинки выделяются бордером и не увеличиваются

--- views/profile/index.php
+++ views/profile/index.php
@@ -9,6 +9,27 @@
 use app\models\db\Like;
 use Yii;
 
+$this->registerCss("
+  .show_img{
+    height: 100px; 
+    border: 1px black solid; 
+    margin: 10px;
+  }
+
+  .show_img:hover{
+    cursor: pointer;
+  border: 1px solid red;
+  }
+   /*
+  .show_img:hover{
+   position: absolute;
+   height: 200px;
+   padding: 30px;
+   background: white;
+   } 
+*/
+");
+
 $user_id = Yii::$app->user->identity->id;
 $likes = Like::find()
              ->where("user_id = $user_id AND action = 1")
@@ -17,7 +38,7 @@ $likes = Like::find()
 $posts = '';
 foreach($likes as $like)
 {
-  echo "<img src='".(Yii::$app->picker->pick($like->post_id))['img']."' style='height: 100px; border: 1px black solid; margin: 10px;'>";
+  echo "<img src='".(Yii::$app->picker->pick($like->post_id))['img']."' class='show_img'>";
 }
 
 
