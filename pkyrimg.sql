36fd2dcb913ff5b28eb87d2a1816c0b5ee7389f2
Serj <kraspvp@mail.ru>
2020-02-27 13:46:56 +0700

    БД очищена и является пустой

--- pkyrimg.sql
+++ pkyrimg.sql
@@ -79,7 +79,7 @@ CREATE TABLE `user` (
   `role` varchar(255) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,6 +88,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'demo','$2y$13$3Eum2bpedRq8yV4c4s32AuP36HyHzguHu1/2VjJCTVaAVnP1DOl36','user');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -100,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-27 13:45:24
+-- Dump completed on 2020-02-27 13:46:49
