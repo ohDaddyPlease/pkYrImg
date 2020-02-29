127f4a2bb322e1359eb3af99f2386ec63a86c766
serj <kraspvp@mail.ru>
2020-02-29 21:14:06 +0700

    Добавлен лоадер, который JS'ом убирается, если картинка прогрузилась

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -40,25 +40,57 @@ $this->registerCss("
  */
 $this->registerJs(
   !Yii::$app->user->isGuest ?
-  "$('#like_button, #dislike_button').click(function(data){
+  "
+  let interval = setInterval(function(){
+    if($('#img_id').prop('complete'))
+    {
+      $('#loader').css('display', 'none');
+      clearInterval(interval);
+    }
+  });
+
+  $('#like_button, #dislike_button').click(function(data){
+    $('#loader').css('display', 'table');
     $.ajax({
       url: '?r=dashboard/like-dislike',
       type: 'POST',
       data: {
-        num: $num,
-        img: '$img',
+        num: $('#img_id').attr('data-num'),
+        img: $('#img_id').attr('src'),
         action: data.target.dataset.action
       },
-      success: function() {
-        location.reload();
+      success: function(e) {
+        e = JSON.parse(e);
+        $('#img_id').attr('src', e['img']);
+        $('#img_id').attr('alt', e['safe_title']);
+        $('#img_id').attr('data-num', e['num']);
+        $('#title').text(e['safe_title']);
+        let interval = setInterval(function(){
+          if($('#img_id').prop('complete')){
+            $('#loader').css('display', 'none');
+            clearInterval(interval);
+          }
+        }, 300);
+        
+        
       },
-      error: function() {
+      error: function(e) {
         console.log('[Форма лайков/дизлайков] Что-то пошло не так...');
+        console.error(e);
       }
     });
     });
     " : 
-    "$('#like_button, #dislike_button').click(function(){
+    "
+    let interval = setInterval(function(){
+      if($('#img_id').prop('complete'))
+      {
+        $('#loader').css('display', 'none');
+        clearInterval(interval);
+      }
+    });
+    
+    $('#like_button, #dislike_button').click(function(){
     $('#need_auth').css('display', 'block');
     });",
   View::POS_READY
@@ -66,9 +98,10 @@ $this->registerJs(
 
 ?>
 
-<div>
-  <h1 style="border-bottom: 2px solid white; margin-bottom: 20px;"><?= Html::encode($title);?></h1>
-  <?= Html::img(Html::encode($img), ['alt' => Html::encode($title)]); ?>
+<div style="position: relative;">
+  <div id="loader" style="border-radius: 5px; width: 100%; height: calc(100% + 2px); position: absolute; background: antiquewhite; border: 10px solid; display: table;"><p style="display: table-cell; vertical-align: middle; text-align: center; font-size: large; font-weight: bolder;"> Пожалуйста, подождите :)</p></div>
+  <h1 style="border-bottom: 2px solid white; margin-bottom: 20px;" id="title"><?= Html::encode($title);?></h1>
+  <?= Html::img(Html::encode($img), ['alt' => Html::encode($title), 'id' => 'img_id', 'data-num' => $num]); ?>
   <div style="display: block; margin-left: auto; margin-right: auto; max-width: 350px; height: 45px; padding-top: 15px;">
   <?= Html::button('Нравится', ['class' => 'like_button', 'id' => 'like_button', 'data-action' => 1, 'style' => 'width: 49%; height: 100%; font-size: medium;']); ?>
   <?= Html::button('Не нравится', ['class' => 'dislike_button', 'id' => 'dislike_button', 'data-action' => 0, 'style' => 'width: 49%; height: 100%; font-size: medium;']); ?>
