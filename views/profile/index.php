238e8ee5b0961e1f3969e714c560cf3a8040e12e
Serj <kraspvp@mail.ru>
2020-03-09 21:51:10 +0700

    Изменена главная страница профиля: указаны ссылки на лайкнутые, дизлайкнутые, избранные посты

--- views/profile/index.php
+++ views/profile/index.php
@@ -10,112 +10,20 @@ use yii\widgets\LinkPager;
 use yii\bootstrap\Modal;
 use yii\web\View;
 use yii\bootstrap\Html;
+use app\models\db\Post;
 
 $this->registerCss("
-  .show_img{
-    height: 100px; 
-    border: 1px black solid; 
-    margin: 10px;
-  }
-
-  .show_img:hover{
-    cursor: pointer;
-  border: 1px solid red;
-  }
-  #dislike_button{
-    border: 2px solid brown;
-    border-radius: 10px;
-    background: white;
-    color: brown;
-  }
-  #dislike_button:hover{
-    background: sandybrown;
-    transition: 0.8s;
-  }
-  #favorite_button{
-    border: 2px solid limegreen;
-    border-radius: 10px;
-    background: white;
-    color: limegreen;
-  }
-  #favorite_button:hover{
-    background: palegreen;
-    transition: 0.8s;
-  }
-
-  .marked{
-    font-weight: bold !important;
-    color: black !important;
-  }
+.link{
+  border: 1px solid;
+  border-radius: 10px;
+  padding: 3px;
+  text-decoration: none !important;
+}
 ");
 
-$this->registerJs("
-$('.show_img').click(function(data){
-  $('#pic-modal .modal-body').html('<img id=\'modal-img\' src=\''+$(this).attr('src')+ '\' data-id='+$(this).data('id')+'>');
-  $('#pic-modal .modal-dialog').width($('#pic-modal .modal-body img')[0].width + 33);
-  $('#pic-modal').modal('show');
-});
-
-$('#dislike_button').click(function(data){
-  $.ajax({
-    url: '?r=dashboard/like-dislike',
-    type: 'POST',
-    data: {
-      num: $('#modal-img').attr('data-id'),
-      img: $('#modal-img').attr('src'),
-      action: 0
-    },
-    success: function(e) {
-      $('#dislike_button').addClass('marked');
-      $('[class=show_img][data-id=\"'+$('#modal-img').attr('data-id')+'\"]').remove()
-    },
-    error: function(e) {
-      console.log('[Кнопки лайка/дизлайка] Что-то пошло не так...');
-      console.error(e);
-    }
-  });
-  });
-
-  $('#favorite_button').click(function(){
-    $.ajax({
-      url: '?r=dashboard/add-to-favorite',
-      type: 'POST',
-      data: {
-        num: $('#modal-img').attr('data-id'),
-      },
-      success: function(){
-        $('#favorite_button').text('В избранном').addClass('marked');
-      },
-      error: function(e){
-        console.log('[Кнопка добавления в избранное] Что-то пошло не так...');
-        console.error(e);
-      }
-    });
-  });
-",
+$this->registerJs("",
 View::POS_READY
 );
-
-Modal::begin([
-  'header' => false,
-  'footer' => 
-              Html::button('Не нравится', ['class' => 'dislike_button', 'id' => 'dislike_button', 'data-action' => 0, 'style' => 'outline: none; width: 50%; height: 100%; font-size: medium;']) . 
-              Html::button('В избранное', ['class' => 'favorite_button', 'id' => 'favorite_button', 'data-action' => 0, 'style' => 'outline: none; width: calc(50% - 10px); margin-left: 10px; height: 100%; font-size: medium;']),
-  'options' => [
-    'id' => 'pic-modal'
-  ],
-  'size' => Modal::SIZE_DEFAULT
-]);
-echo 'test';
-Modal::end();
-
-foreach ($models as $model) {
-  echo "<img src='".($model->img ?? 'https://www.bafe.org.uk/imgs/icons/x-mark-256x256-red.png')."' data-id='$model->post_id' class='show_img'>";
-}
-
-echo "<br>" . LinkPager::widget([
-  'pagination' => $pages,
-  'id' => 'pagination'
-]);
-
 ?>
+
+Пс-с-с, пользователь с логином <?= Yii::$app->user->identity->login; ?>, а ты знаешь, что у тебя <a href='?r=profile/likes' class='link'> лайкнутых</a> постов <?= Post::find()->where(['action' => 1])->count(); ?>, <a href='?r=profile/dislikes' class='link'> дизлайкнутых</a> <?= Post::find()->where(['action' => 0])->count(); ?> и постов <a href='?r=profile/favorites' class='link'>в избранном</a> <?= Post::find()->where(['favorite' => 1])->count(); ?> ?
