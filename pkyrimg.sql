8e5bf9aa046d40404b57133cd529cd519ddcac67
Serj <kraspvp@mail.ru>
2020-03-09 21:54:43 +0700

    Убраны лишние таблицы и объеденены в одну - Post

--- pkyrimg.sql
+++ pkyrimg.sql
@@ -40,51 +40,31 @@ LOCK TABLES `assessment` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `favorite`
+-- Table structure for table `post`
 --
 
-DROP TABLE IF EXISTS `favorite`;
+DROP TABLE IF EXISTS `post`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `favorite` (
-  `user` int(11) DEFAULT NULL,
-  `post` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `favorite`
---
-
-LOCK TABLES `favorite` WRITE;
-/*!40000 ALTER TABLE `favorite` DISABLE KEYS */;
-/*!40000 ALTER TABLE `favorite` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `like`
---
-
-DROP TABLE IF EXISTS `like`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `like` (
+CREATE TABLE `post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `post_id` int(11) DEFAULT NULL,
   `action` tinyint(1) DEFAULT NULL,
   `img` char(150) DEFAULT NULL,
+  `favorite` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `like`
+-- Dumping data for table `post`
 --
 
-LOCK TABLES `like` WRITE;
-/*!40000 ALTER TABLE `like` DISABLE KEYS */;
-/*!40000 ALTER TABLE `like` ENABLE KEYS */;
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,1,219,0,'https://imgs.xkcd.com/comics/blanket_fort.png',NULL),(2,1,1231,1,'https://imgs.xkcd.com/comics/habitable_zone.png',0),(3,1,664,0,'https://imgs.xkcd.com/comics/academia_vs_business.png',NULL),(4,1,1116,0,'https://imgs.xkcd.com/comics/traffic_lights.gif',NULL),(5,1,1823,1,'https://imgs.xkcd.com/comics/hottest_editors.png',1),(6,1,1124,0,'https://imgs.xkcd.com/comics/law_of_drama.png',0),(7,1,54,0,'https://imgs.xkcd.com/comics/science.jpg',0),(8,1,981,0,'https://imgs.xkcd.com/comics/porn_folder.png',1),(9,1,1969,0,'https://imgs.xkcd.com/comics/not_available.png',0),(10,1,1912,NULL,NULL,0),(11,1,1847,0,'https://imgs.xkcd.com/comics/dubious_study.png',1),(12,1,1209,NULL,NULL,0),(13,1,932,NULL,'https://imgs.xkcd.com/comics/cia.png',0),(14,1,1497,NULL,'https://imgs.xkcd.com/comics/new_products.png',0),(15,1,1972,NULL,'https://imgs.xkcd.com/comics/autogyros.png',0),(16,1,1999,NULL,'https://imgs.xkcd.com/comics/selection_effect.png',0),(17,1,1089,NULL,'https://imgs.xkcd.com/comics/internal_monologue.png',0),(18,1,621,0,'https://imgs.xkcd.com/comics/superlative.png',1),(19,1,1956,NULL,'https://imgs.xkcd.com/comics/unification.png',0),(20,1,1357,0,'https://imgs.xkcd.com/comics/free_speech.png',NULL),(21,1,915,0,'https://imgs.xkcd.com/comics/connoisseur.png',NULL),(22,1,1100,1,'https://imgs.xkcd.com/comics/vows.png',0);
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -123,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-02 13:48:06
+-- Dump completed on 2020-03-09 20:15:21
